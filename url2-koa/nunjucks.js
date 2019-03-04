532e3e03b3d9b583d0f09231492fbb91d05caf2a
kingmore96 <wgg9696@163.com>
2019-03-04 13:27:47 +0800

    第二次修改，新增mvc学习部分

---
+++ url2-koa/nunjucks.js
@@ -0,0 +1,20 @@
+const nunjucks = require('nunjucks');
+
+var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views',{
+    watch:true,
+    //开发环境设置为true;生产环境,设置为false,避免每次都同步读取模板，直接使用内存中的缓存
+    noCache:true
+}));
+
+var s = env.render('hello.html',{
+    fruits:[
+        "apple",
+        "pear"
+    ]
+});
+
+var s1 = env.render('extend.html',{
+    head:"Hello",
+    body:"bla bla bla ......"
+})
+console.log(s1);
---
+++ view-koa/app.js
@@ -0,0 +1,21 @@
+const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+
+const routes = require('./controller');
+
+const app = new Koa();
+
+app.use(async(ctx,next) =>{
+    console.log(`Process ${ctx.request.method},${ctx.request.url}......`);
+    await next();    
+});
+
+app.use(bodyParser());
+
+app.use(routes);
+
+app.listen(3000);
+
+console.log("开始监听啦！");
+
